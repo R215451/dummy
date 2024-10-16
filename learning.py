c61e3c2a45b19178f792b9fbf102300c5f4b0d62
R215451 <moin9166766547@gmail.com>
2024-10-16 17:11:27 +0530

    Range concept in python

--- learning.py
+++ learning.py
@@ -1,9 +1,62 @@
 # User input in python 
-name = input("Enter your name :")
-print("My name is ",name)
+# name = input("Enter your name :")
+# print("My name is ",name)
 
-age = int(input("Enter your age ")) # age is integer and input function returs string , so we have to type convert from str to int
-print("My age is ",age)
+# age = int(input("Enter your age ")) # age is integer and input function returs string , so we have to type convert from str to int
+# print("My age is ",age)
 
-marks = float(input("Enter your marks "))
-print(marks)
+# marks = float(input("Enter your marks "))
+# print(marks)
+
+# name = input("Enter your name :")
+# print("my name is ",name)
+
+# age = int(input('Enter your age'))
+# print('my age is',age )
+
+# marks = float (input("Enter your marks"))
+# print(marks)
+
+list = ['mohiz','ahan']
+
+print(list)
+
+for name in list:
+    print(name)
+
+res = list[1]
+print(res)
+
+list.append('nufair')
+
+print(list)
+
+list.remove('ahan')
+
+print('ahan')
+
+
+
+#range(7)# 0 1 2 3 4 5 6 
+
+
+
+
+for values in range(7):
+    print(values," ")
+    print()
+
+for values in range( 1,18):
+    print(values,)
+
+
+for data in range(3,19):
+
+    print(data," ")
+
+
+
+for data in range(0,39,3):
+
+    print(data)
+    
