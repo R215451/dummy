07416217cd56a09e5848dac825149c2200fc167e
R215451 <moin9166766547@gmail.com>
2024-10-16 09:32:36 +0530

    Update learning.py

--- learning.py
+++ learning.py
@@ -2,5 +2,8 @@
 name = input("Enter your name :")
 print("My name is ",name)
 
-age = int(input("Enter your age")) # age is integer and input function returs string , so we have to type convert from str to int
+age = int(input("Enter your age ")) # age is integer and input function returs string , so we have to type convert from str to int
 print("My age is ",age)
+
+marks = float(input("Enter your marks "))
+print(marks)
