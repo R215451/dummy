7719cdc16d4aa0ae94edbde4ad968ebc18d701b1
R215451 <moin9166766547@gmail.com>
2024-10-16 09:29:28 +0530

    learning python

---
+++ learning.py
@@ -0,0 +1,6 @@
+# User input in python 
+name = input("Enter your name :")
+print("My name is ",name)
+
+age = int(input("Enter your age")) # age is integer and input function returs string , so we have to type convert from str to int
+print("My age is ",age)
