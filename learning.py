1df44c854e91d010253e8169be2fd4555c2c9867
R215451 <moin9166766547@gmail.com>
2024-10-17 18:58:19 +0530

    attendence

--- learning.py
+++ learning.py
@@ -1,62 +1,105 @@
-# User input in python 
-# name = input("Enter your name :")
-# print("My name is ",name)
+# # User input in python 
+# # name = input("Enter your name :")
+# # print("My name is ",name)
 
-# age = int(input("Enter your age ")) # age is integer and input function returs string , so we have to type convert from str to int
-# print("My age is ",age)
+# # age = int(input("Enter your age ")) # age is integer and input function returs string , so we have to type convert from str to int
+# # print("My age is ",age)
 
-# marks = float(input("Enter your marks "))
-# print(marks)
+# # marks = float(input("Enter your marks "))
+# # print(marks)
 
-# name = input("Enter your name :")
-# print("my name is ",name)
+# # name = input("Enter your name :")
+# # print("my name is ",name)
 
-# age = int(input('Enter your age'))
-# print('my age is',age )
+# # age = int(input('Enter your age'))
+# # print('my age is',age )
 
-# marks = float (input("Enter your marks"))
-# print(marks)
+# # marks = float (input("Enter your marks"))
+# # print(marks)
 
-list = ['mohiz','ahan']
+# list = ['mohiz','ahan']
 
-print(list)
+# print(list)
 
-for name in list:
-    print(name)
+# for name in list:
+#     print(name)
 
-res = list[1]
-print(res)
+# res = list[1]
+# print(res)
 
-list.append('nufair')
+# list.append('nufair')
 
-print(list)
+# print(list)
 
-list.remove('ahan')
+# list.remove('ahan')
 
-print('ahan')
-
-
-
-#range(7)# 0 1 2 3 4 5 6 
+# print('ahan')
 
 
 
-
-for values in range(7):
-    print(values," ")
-    print()
-
-for values in range( 1,18):
-    print(values,)
-
-
-for data in range(3,19):
-
-    print(data," ")
+# #range(7)# 0 1 2 3 4 5 6 
 
 
 
-for data in range(0,39,3):
 
-    print(data)
+# for values in range(7):
+#     print(values," ")
+#     print()
+
+# for values in range( 1,18):
+# #     print(values,)
+
+
+# # for data in range(3,19):
+
+# #     print(data," ")
+
+
+
+# # for data in range(0,39,3):
+
+#     # print(data)
     
+# list =['mohis']
+
+# print(list)
+
+# for name in list : 
+#     print(name)
+
+# res = list[0]
+
+# print(res)
+
+# list.append('aahil')
+
+# print(list)
+
+# list.remove('aahil')
+
+# print(list)
+
+
+list_numbers = [10,40,77,50,7]
+
+for numbers in list_numbers:
+    if numbers == 77:
+        print("Moise")
+    
+print()
+
+print("Sorting list in ascending order")
+list_numbers.sort()        
+for d in list_numbers:
+    print(d)         
+
+
+
+list_student_attendence = ['present','absent']
+
+name_moise = ''
+name_moise = list_student_attendence[0]
+for attendence in list_student_attendence:
+    if attendence == name_moise:
+        print("Moise is present")
+   
