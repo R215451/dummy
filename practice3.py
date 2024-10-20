e3b87df4fd535f8838f772436f4923b1610c4844
R215451 <moin9166766547@gmail.com>
2024-10-20 15:01:19 +0530

    update

--- practice3.py
+++ practice3.py
@@ -1,48 +1,102 @@
-# remove all white spaces from given string 
-name = "abc cde cdfscgcbhs "
-print(name.replace(' ',''))
+# # remove all white spaces from given string 
+# name = "abc cde cdfscgcbhs "
+# print(name.replace(' ',''))
 
-# remove all  from lower to upper or viceversa  
-name_1 = "abc cde cdfscgcbhs ".upper()
-print(name_1)# Converted in upper case
+# # remove all  from lower to upper or viceversa  
+# name_1 = "abc cde cdfscgcbhs ".upper()
+# print(name_1)# Converted in upper case
 
-#replace the spaces of a string with a specific character
-name_2 = input('Enter your name ')
-new_str = name_2.replace(' ','@')
-print(new_str)
-#find the duplicate characters in a string
-name_3 = input('Enter your name ')
-name_3_3 = ''
-for item_char in name_3:
-    if item_char not in name_3_3:
-        name_3_3 = name_3_3+item_char
+# #replace the spaces of a string with a specific character
+# name_2 = input('Enter your name ')
+# new_str = name_2.replace(' ','@')
+# print(new_str)
+# #find the duplicate characters in a string
+# name_3 = input('Enter your name ')
+# name_3_3 = ''
+# for item_char in name_3:
+#     if item_char not in name_3_3:
+#         name_3_3 = name_3_3+item_char
     
-print(name_3)
-print(name_3_3)    
+# print(name_3)
+# print(name_3_3)    
 
-#find the duplicate words in a string
-name_4  = "ABC BCD ABC"
-empty_list = []
-list_name = name_4.split()
-for i in list_name:
-    if i not in empty_list:
-        empty_list.append(i)
-print(' '.join(empty_list))    
+# #find the duplicate words in a string
+# name_4  = "ABC BCD ABC"
+# empty_list = []
+# list_name = name_4.split()
+# for i in list_name:
+#     if i not in empty_list:
+#         empty_list.append(i)
+# print(' '.join(empty_list))    
 
-#find frequency of characters in python 
-name_5 = "abcvhvcsahvcskhkbcsk"
-frequency = {}
-for i in name_5:
-    if i in frequency:
-        frequency[i] = frequency[i]+1
-    else:
-        frequency[i] = 1
+# #find frequency of characters in python 
+# name_5 = "abcvhvcsahvcskhkbcsk"
+# frequency = {}
+# for i in name_5:
+#     if i in frequency:
+#         frequency[i] = frequency[i]+1
+#     else:
+#         frequency[i] = 1
 
-print(frequency)
+# print(frequency)
 
-# Swap of two strings in python without using third variable 
-name_6 = input('Enter your name ')
-name_7 = input('Enter your name ')
-print(name_6,name_7)
-name_6, name_7 = name_7,name_6
-print(name_6,name_7)
+# # Swap of two strings in python without using third variable 
+# name_6 = input('Enter your name ')
+# name_7 = input('Enter your name ')
+# print(name_6,name_7)
+# name_6, name_7 = name_7,name_6
+# print(name_6,name_7)
+
+# #Find the sum of natural numbers  1 2 3 4 5 
+# sum_natural_numbers =0
+# for i in range(1,101):
+#     sum_natural_numbers = sum_natural_numbers + i
+# print(sum_natural_numbers)
+
+# #Find the sum of list numbers 
+# list_1 = [10,20,4,77,7,87,45]
+# sum_of_list_numbers  = 0
+# for i in list_1:
+#    sum_of_list_numbers = sum_of_list_numbers + i
+# print(sum_of_list_numbers)
+
+# divide string in n equal parts 
+divide_string = "abcdefghijk"
+number_of_division = 3
+l1 = len(divide_string)
+part_length = l1 // number_of_division
+remender = l1 % number_of_division
+
+parts = []
+start = 0
+for i in range(number_of_division):
+    end = start + part_length + (1 if remender > 0 else 0)
+    parts.append(divide_string[start:end])
+    start = end
+    remender = remender -1 
+
+print(parts) 
+
+
+# Define two sets
+set_a = {1, 2, 3}
+set_b = {1, 2, 3, 4, 5}
+
+# Check if set_a is a subset of set_b
+is_subset = set_a.issubset(set_b)
+
+print(is_subset)
+
+string = "abc"
+subsets = [""]
+for char in string:
+  new_subsets = []
+  for subset in subsets:
+     new_subset = subset+char
+     new_subsets.append(new_subset)
+
+  subsets  = subsets+new_subsets 
+
+print(subsets) 
+ 
+
--- testing.py
+++ testing.py
@@ -1,38 +1,54 @@
-import string
+# import string
 
-import math,string
+# import math,string
 
-from num2words import num2words
+# from num2words import num2words
 
-name = "abc 123"
-new_list = name.split()
-print(new_list)
+# name = "abc 123"
+# new_list = name.split()
+# print(new_list)
 
-reverse_list = new_list[::-1]
-print(reverse_list)
+# reverse_list = new_list[::-1]
+# print(reverse_list)
 
-print(' '. join(reverse_list))
-user_number = 0
-while True:
-    try:
+# print(' '. join(reverse_list))
+# user_number = 0
+# while True:
+#     try:
 
-        user_number = int(input('Enter a number as your wish: '))
-        break  # Exit the loop if input is valid
-    except ValueError:
-        print("That's not a valid number. Please try again.")
+#         user_number = int(input('Enter a number as your wish: '))
+#         break  # Exit the loop if input is valid
+#     except ValueError:
+#         print("That's not a valid number. Please try again.")
        
-converted_number = num2words(user_number)
-print(converted_number)
+# converted_number = num2words(user_number)
+# print(converted_number)
 
 
-# Anagrams of two strings 
-str1 = input('Enter a string 1 ')
-str2 = input('Enter a string 2 ')
+# # Anagrams of two strings 
+# str1 = input('Enter a string 1 ')
+# str2 = input('Enter a string 2 ')
 
-str1 = str1.replace(' ','').lower()
-str2 = str2.replace(' ','').lower()
+# str1 = str1.replace(' ','').lower()
+# str2 = str2.replace(' ','').lower()
 
-if sorted(str1) == sorted(str2):
-    print('Anagrams')
-else:
-    print('Not Anagrams')
+# if sorted(str1) == sorted(str2):
+#     print('Anagrams')
+# else:
+#     print('Not Anagrams')
+
+divide_string = input('Enter your name ')
+number_of_division = int(input('Entre a number in which do you want to divide your name '))
+l1 = len(divide_string)
+part_length = l1 // number_of_division
+remender = l1 % number_of_division
+
+parts = []
+start = 0
+for i in range(number_of_division):
+     end = start + part_length + (1 if remender > 0 else 0)
+     parts.append(divide_string[start:end]) 
+     start = end
+     remender = remender - 1
+
+print(parts)
